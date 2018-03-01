4575dfb9de4333f71c393fc214431f2a443315e1
Misza Lemko <32789786+miszalemko@users.noreply.github.com>
2018-03-01 23:39:39 +0100

    Update Runner.java
    Adding method for finding prime numbers.
    Trying to make count '3' of prime numbers using for and random range of numbers

--- lecture32/src/Runner.java
+++ lecture32/src/Runner.java
@@ -1,5 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
+        int count = 0;
+
       for(int i = 2;i < 9; i++) {
           System.out.println(calculateInterest(10000,i));
       }
@@ -7,6 +9,27 @@ public class Runner {
       for(int i = 8; i > 1; i--) {
           System.out.println(calculateInterest(10000,i));
       }
+
+      for (int i = 1;i < 100;i++) {
+          if(isPrime(i)){
+              count++;
+              if (count == 3) {
+                  break;
+              }
+          }
+      }
+    }
+
+    public static boolean isPrime(int n){
+        if (n==1) {
+            return false;
+        }
+        for(int i = 2;i <=n/2;i++) {
+            if (n%i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
